f3aa5c0be7cce56c5fab93ed37f917b58b7887b3
edeniarp <zyneticartz@gmail.com>
2024-08-21 17:03:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const projectDescription = document.getElementById('project-description');
     const backButton = document.getElementById('back-button');
     const mainContent = document.getElementById('main-content');
+    
+    const profileImg = document.getElementById('profile-img');
+    const profileModal = document.getElementById('profile-modal');
 
+    // Gère le clic sur les projets pour afficher les détails
     projects.forEach(project => {
         project.addEventListener('click', () => {
             const projectId = project.getAttribute('data-project');
-            // Configure les détails du projet ici
             projectImage.src = `https://via.placeholder.com/600x400?text=Image+du+Projet+${projectId}`;
             projectDescription.textContent = `Description détaillée du projet ${projectId}. Voici plus d'informations sur ce projet.`;
             
-            // Montre les détails du projet
             mainContent.classList.add('hidden');
             projectDetails.classList.add('show');
         });
     });
 
+    // Gère le clic sur le bouton de retour
     backButton.addEventListener('click', () => {
-        // Cache les détails du projet et montre le contenu principal
         projectDetails.classList.remove('show');
         mainContent.classList.remove('hidden');
+    });
+
+    // Gère le clic sur la photo de profil pour afficher le modal
+    profileImg.addEventListener('click', () => {
+        profileModal.classList.add('show');
+    });
+
+    // Gère le clic en dehors du modal de la photo de profil pour le fermer
+    profileModal.addEventListener('click', (event) => {
+        if (event.target === profileModal) {
+            profileModal.classList.remove('show');
+        }
     });
 });
